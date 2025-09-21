a38895cace699ea7abb1483c7ac65ef5637ad654
Nimbus Chaubey <dugguu1056@gmail.com>
2025-09-21 13:21:38 +0530

    Day 1: Sum of two numbers program
    Added Day 1 code for input two numbers and sum
    Day 1: Two number addition program

---
+++ Day1a.c
@@ -0,0 +1,16 @@
+//Write a program to input two numbers and display their sum.
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter first value:\n");
+    scanf("%d",&a);
+    printf("enter second value:\n");
+    scanf("%d",&b);
+    c= a + b;
+
+    printf("%d",c);
+    return 0;
+}
+
+
