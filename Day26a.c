471d31eeb3cd70fbfd9b70795488283be98942fd
Nimbus Chaubey <dugguu1056@gmail.com>
2025-10-06 11:54:56 +0530

    paatter for stars
    pattern for stars

---
+++ Day26a.c
@@ -0,0 +1,20 @@
+//Write a program to print the following pattern
+#include <stdio.h>
+int main() 
+{
+    for (int i = 5; i >= 1; i--) 
+    {
+        
+        for (int s = 1; s < i; s++) 
+        {
+            printf(" ");
+        }
+       
+        for (int j = i; j <= 5; j++) 
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
