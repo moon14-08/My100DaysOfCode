9981282c7f5b3d2762161fcbcca5062e1a075a12
Nimbus Chaubey <dugguu1056@gmail.com>
2025-11-15 11:17:41 +0530

    prime no. and one-dimensional array
    a)Write a program to print all the prime numbers from 1 to n.
    b)Read and print elements of a one-dimensional array.

---
+++ Day28b.c
@@ -0,0 +1,23 @@
+//Read and print elements of a one-dimensional array.
+#include <stdio.h>
+int main()
+{
+    int n;   
+    printf("Enter number of elements:\n");
+    scanf("%d", &n);
+
+     int arr[n];
+  
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+   
+    printf("Array elements are:\n");
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
